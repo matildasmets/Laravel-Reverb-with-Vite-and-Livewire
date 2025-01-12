2d0b82e2119516af2ac5e9d738ec89f1733b5379
issatillie <uarpnn@gmail.com>
2025-01-12 15:16:35 +0100

    Removal double 'echo.js'

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel Reverb Livewire Example</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/echo.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 <body class="antialiased">
